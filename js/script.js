610ee6181e064b042b30f6866bfbbd8abb989433
Alexey Koshevoy <gphtechak@gmail.com>
2019-02-20 16:34:18 +0200

    js connected

--- js/script.js
+++ js/script.js
@@ -1,21 +1,49 @@
-var buttonSub = document.getElementById('id-adults-sub');
-var buttonAdd = document.getElementById('id-adults-add');
-var valueSub;
 
+// var popup = document.querySelector('.booking-form');
+// popup.classList.add('form-show');
+var button = document.querySelector('.booking-CTA-button');
+button.addEventListener('click', function () {
+    console.log('Клик по кнопке');
+});
 
-buttonSub.addEventListener('click', function (evt) {
+var popup = document.querySelector('.modal');
+var openPopupButton = document.querySelector('.button-open');
+var closePopupButton = popup.querySelector('.button-close');
+
+openPopupButton.addEventListener('click', function (evt) {
     evt.preventDefault();
-    valueSub = document.getElementById('id-adults').value;
-    if (valueSub>1) {
-        document.getElementById('id-adults').value = valueSub - 1;
-    }
+    popup.classList.add('modal--show');
+});
+
+closePopupButton.addEventListener('click', function () {
+    popup.classList.remove('modal--show');
 });
 
 
-buttonAdd.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    valueSub = document.getElementById('id-adults').value;
-    if (valueSub<8) {
-        document.getElementById('id-adults').value = Number(valueSub) + 1;
-    }
-});
+
+
+
+
+
+
+// var buttonSub = document.getElementById('id-adults-sub');
+// var buttonAdd = document.getElementById('id-adults-add');
+// var valueSub;
+//
+//
+// buttonSub.addEventListener('click', function (evt) {
+//     evt.preventDefault();
+//     valueSub = document.getElementById('id-adults').value;
+//     if (valueSub>1) {
+//         document.getElementById('id-adults').value = valueSub - 1;
+//     }
+// });
+//
+//
+// buttonAdd.addEventListener('click', function (evt) {
+//     evt.preventDefault();
+//     valueSub = document.getElementById('id-adults').value;
+//     if (valueSub<8) {
+//         document.getElementById('id-adults').value = Number(valueSub) + 1;
+//     }
+// });
