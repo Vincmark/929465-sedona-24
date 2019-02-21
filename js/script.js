f36edafc37332471aad28def062d174c8b5a5f29
Alexey Koshevoy <gphtechak@gmail.com>
2019-02-21 03:28:27 +0200

    js functionality added to index.html

--- js/script.js
+++ js/script.js
@@ -1,49 +1,57 @@
 
-// var popup = document.querySelector('.booking-form');
-// popup.classList.add('form-show');
-var button = document.querySelector('.booking-CTA-button');
-button.addEventListener('click', function () {
-    console.log('Клик по кнопке');
-});
-
-var popup = document.querySelector('.modal');
-var openPopupButton = document.querySelector('.button-open');
-var closePopupButton = popup.querySelector('.button-close');
-
-openPopupButton.addEventListener('click', function (evt) {
+let button = document.querySelector('.booking-CTA-button');
+let booking_form = document.querySelector('.booking-form');
+button.addEventListener('click', function (evt) {
     evt.preventDefault();
-    popup.classList.add('modal--show');
+    let isFormOpened=booking_form.classList.contains('form-show');
+    if (isFormOpened) {
+        booking_form.classList.remove('form-show');
+    } else {
+        booking_form.classList.add('form-show');
+    }
 });
 
-closePopupButton.addEventListener('click', function () {
-    popup.classList.remove('modal--show');
+
+let adultsAddBtn=document.getElementById('id-adults-add');
+adultsAddBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    let adultsNum=document.getElementById('id-adults');
+    let num=adultsNum.value;
+    num++;
+    if (num<=9) {
+        adultsNum.value=num;
+    }
 });
 
+let adultsSubBtn=document.getElementById('id-adults-sub');
+adultsSubBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    let adultsNum=document.getElementById('id-adults');
+    let num=adultsNum.value;
+    num--;
+    if (num>=1) {
+        adultsNum.value=num;
+    }
+});
 
+let childrenAddBtn=document.getElementById('id-children-add');
+childrenAddBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    let childrenNum=document.getElementById('id-children');
+    let num=childrenNum.value;
+    num++;
+    if (num<=9) {
+        childrenNum.value=num;
+    }
+});
 
-
-
-
-
-
-// var buttonSub = document.getElementById('id-adults-sub');
-// var buttonAdd = document.getElementById('id-adults-add');
-// var valueSub;
-//
-//
-// buttonSub.addEventListener('click', function (evt) {
-//     evt.preventDefault();
-//     valueSub = document.getElementById('id-adults').value;
-//     if (valueSub>1) {
-//         document.getElementById('id-adults').value = valueSub - 1;
-//     }
-// });
-//
-//
-// buttonAdd.addEventListener('click', function (evt) {
-//     evt.preventDefault();
-//     valueSub = document.getElementById('id-adults').value;
-//     if (valueSub<8) {
-//         document.getElementById('id-adults').value = Number(valueSub) + 1;
-//     }
-// });
+let childrenSubBtn=document.getElementById('id-children-sub');
+childrenSubBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    let childrenNum=document.getElementById('id-children');
+    let num=childrenNum.value;
+    num--;
+    if (num>=0) {
+        childrenNum.value=num;
+    }
+});
